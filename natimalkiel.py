945b49bbb37fdf98a24853f2939210ef94c1f02f
netanelmalkiel <nati.malkiel@gmail.com>
2022-06-14 02:39:30 +0300

    wepekaya mother fucker

---
+++ natimalkiel.py
@@ -0,0 +1,14 @@
+for number in range(1, 31):
+    if number % 3 == 0 and number % 5 == 0:
+        print('nati malkiel')
+    elif number % 3 == 0:
+        print('nati')
+    elif number % 5 == 0:
+        print('malkiel')
+    else:
+        print(number)
+
+
+
+
+
