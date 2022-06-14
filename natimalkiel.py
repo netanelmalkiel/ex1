641cb17fd26ad5a6964868e2672470d84c5607da
netanelmalkiel <nati.malkiel@gmail.com>
2022-06-14 10:05:00 +0300

    change to function

--- natimalkiel.py
+++ natimalkiel.py
@@ -1,14 +1,16 @@
-for number in range(1, 31):
-    if number % 3 == 0 and number % 5 == 0:
-        print('nati malkiel')
-    elif number % 3 == 0:
-        print('nati')
-    elif number % 5 == 0:
-        print('malkiel')
-    else:
-        print(number)
+def nati_mal(s_num, e_num):
+    for number in range(s_num, e_num):
+        if number % 3 == 0 and number % 5 == 0:
+            print('nati malkiel')
+        elif number % 3 == 0:
+            print('nati')
+        elif number % 5 == 0:
+            print('malkiel')
+        else:
+            print(number)
 
 
+nati_mal(1, 31)
 
 
 
