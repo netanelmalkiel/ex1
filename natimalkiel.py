3b177bf2fcf831a6a20fa674c65c0076742c3e2a
netanelmalkiel <nati.malkiel@gmail.com>
2022-06-14 12:16:27 +0300

    add a user input

--- natimalkiel.py
+++ natimalkiel.py
@@ -1,5 +1,5 @@
-def nati_mal(s_num, e_num):
-    for number in range(s_num, e_num):
+def nati_mal(start, end):
+    for number in range(starting_num, ending_num):
         if number % 3 == 0 and number % 5 == 0:
             print('nati malkiel')
         elif number % 3 == 0:
@@ -10,7 +10,23 @@ def nati_mal(s_num, e_num):
             print(number)
 
 
-nati_mal(1, 31)
+starting_num = int(input("give me a starting number: "))
+ending_num = int(input("give me a ending number: "))
+
+nati_mal(starting_num, ending_num)
 
 
 
+# def nati_mal(s_num, e_num):
+#     for number in range(s_num, e_num):
+#         if number % 3 == 0 and number % 5 == 0:
+#             print('nati malkiel')
+#         elif number % 3 == 0:
+#             print('nati')
+#         elif number % 5 == 0:
+#             print('malkiel')
+#         else:
+#             print(number)
+#
+#
+# nati_mal(1, 31)
