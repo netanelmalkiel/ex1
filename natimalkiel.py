d51e0bc4cda50bbd9ac56888bb2b54279114c009
netanelmalkiel <nati.malkiel@gmail.com>
2022-06-16 11:21:21 +0300

    change layout

--- natimalkiel.py
+++ natimalkiel.py
@@ -1,59 +1,34 @@
-# check digits
-#
-def check_digits():
-    number = str(input("please give me the first number: "))
+import sys
 
-    if number.isdigit():
+
+def nati_malkiel(range_first_number, range_second_number):
+    number_1_to_check = str(input("give me the first number to check the division: "))
+    if number_1_to_check.isdigit():
         print("ok")
     else:
-        try_one = input(f"please give me a number, {number}! is not a number: ")
-        if try_one.isdigit():
-            print("ok")
-        else:
-            print("program end")
-
-    number2 = str(input("please give me the second number: "))
-
-    if number2.isdigit():
+        print("error")
+        (input("give me the first number to check the division: "))
+        sys.exit()
+    number_2_to_check = str(input("give me the second number to check the division: "))
+    if number_2_to_check.isdigit():
         print("ok")
     else:
-        try_one = input(f"please give me a number, {number2}! is not a number: ")
-        if try_one.isdigit():
-            print("ok")
-        else:
-            print("program end")
-#
-#
-# check_digits()
+        print("error")
+        str(input("give me the second number to check the division: "))
+        sys.exit()
 
-# range to check
-
-
-def range_to_check(f_num, s_num):
-
-    for x in range(f_num, s_num):
-        print(x)
-
-# numbers to check
-
-
-def nati_mal(num3, num4):
-    for x in range(1, 100):
-        if x % num3 == 0 and x % num4 == 0:
+    for number in range(range_first_number, range_second_number):
+        if number % int(number_1_to_check) == 0 and number % int(number_2_to_check) == 0:
             print('nati malkiel')
-        elif x % num3 == 0:
+        elif number % int(number_1_to_check) == 0:
             print('nati')
-        elif x % num4 == 0:
+        elif number % int(number_2_to_check) == 0:
             print('malkiel')
         else:
-            print(x)
+            print(number)
 
 
-
-
-
-
-
+(nati_malkiel(1, 101)) # למה אני צריך לשים פה הוא לא עושה כלום
 
 
 
