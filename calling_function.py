7a9d5f7af83bcfe086c3fe57d1dccfa1b9293fac
netanelmalkiel <nati.malkiel@gmail.com>
2022-06-20 01:23:03 +0300

    adding calling... and __main__ to natimalkiel

---
+++ calling_function.py
@@ -0,0 +1,3 @@
+import natimalkiel
+
+natimalkiel.main()
--- natimalkiel.py
+++ natimalkiel.py
@@ -1,17 +1,19 @@
 import sys
 
-def numbers_to_check(fun):
-    number = input(f"give me the {fun} number: ")
+
+def numbers_to_check(position):
+    number = input(f"give me the {position} number: ")
     if not number.isdigit():
-         number = numbers_to_check(fun)
+        number = numbers_to_check(position)
 
     return number
+
 
 num1 = numbers_to_check("first")
 num2 = numbers_to_check("second")
 
 
-def the_answer(first_number, second_number):
+def the_answer():
     for number in range(1, 101):
         if number % int(first_number) == 0 and number % int(second_number) == 0:
             print('nati malkiel')
@@ -23,11 +25,22 @@ def the_answer(first_number, second_number):
             print(int(number))
 
 
+first_number = num1
+second_number = num2
+
+
 def writing(file_name):
     sys.stdout = open(file_name, 'w+')
-    the_answer(num1, num2)
+    the_answer()
+
+
 filename = input("name for the file: ")
 
-the_answer(num1, num2)
-writing(filename)
 
+def main():
+    the_answer()
+    writing(filename)
+
+
+if __name__ == '__main__':
+    main()
