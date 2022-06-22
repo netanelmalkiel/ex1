acdfd8ebdd31e517aaeeb060713dd6b1ca7f364b
netanelmalkiel <nati.malkiel@gmail.com>
2022-06-22 23:44:54 +0300

    improving

--- with_parser.py
+++ with_parser.py
@@ -4,14 +4,16 @@ import sys
 
 def main():
     parser = argparse.ArgumentParser(description='dividing numbers')
-    parser.add_argument('--first', type=int, required=True, help='enter the first number')
-    parser.add_argument('--second', type=int, required=True, help='enter the second number')
+    parser.add_argument('-f', '--first', type=int, required=True, help='enter the first number')
+    parser.add_argument('-s', '--second', type=int, required=True, help='enter the second number')
+    parser.add_argument('-fn', '--filename', type=str, required=True, help='enter the file name')
     args = parser.parse_args()
-    sys.stdout.write(str(the_answer(args)))
+    the_answer(args)
+    writing(args)
 
 
 def the_answer(args):
-    for number in range(1, 31):
+    for number in range(1, 501):
         if number % int(args.first) == 0 and number % int(args.second) == 0:
             print('nati malkiel')
         elif number % int(args.first) == 0:
@@ -22,13 +24,10 @@ def the_answer(args):
             print(int(number))
 
 
-def writing(file_name):
-    sys.stdout = open(file_name, 'w+')
-    main()
+def writing(args):
+    sys.stdout = open(args.filename, 'w+')
+    the_answer(args)
 
-
-filename = input("name for the file: ")
 
 if __name__ == '__main__':
     main()
-    writing(filename)
